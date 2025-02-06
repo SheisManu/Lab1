55cc913e5f926d24a713438974a62867b4f802af
Manuela Martinez <est.manuela.martin@unimilitar.edu.co>
2025-02-05 22:34:04 -0500

    Calculando la media de la señal

--- Analisiseñl.py
+++ Analisiseñl.py
@@ -8,15 +8,24 @@ data = np.loadtxt('emg_neuropathy.txt', delimiter=' ')
 time = data[:, 0]
 signal = data[:, 1]
 
-#PARTE 1 - GRÁFIICA DE LA SEÑAL
+#PARTE 1 - GRÁFICA DE LA SEÑAL
 # Create the plot
 plt.figure(figsize=(10, 5))
 plt.plot(time, signal, label='EMG Signal')
+
+#PARTE 2 - ESTADÍSTICOS DESCRIPTIVOS
+
+#CON PYTHON
+#Media de la señal
+signal_mean = np.mean(signal)
+#Add the mean value as text on the plot
+plt.text(0.05, 0.95, f'Media de la señal: {signal_mean:.2f}', transform=plt.gca().transAxes,
+         fontsize=12, verticalalignment='top', bbox=dict(facecolor='white', alpha=0.5))
+
+#Mostrando la gráfica de la PARTE 1
 plt.xlabel('Time')
 plt.ylabel('Signal')
 plt.title('EMG Neuropathy Signal')
 plt.legend()
 plt.grid(True)
 plt.show()
-
-#PARTE  
